4a12a4e24d6b950009cad834157811949cb5c6ab
imcheran <kuttiavt@gmail.com>
2026-01-12 16:17:14 +0530

    Final: Add CSS and fix index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';  // 👈 ADD THIS LINE (Loads Tailwind)
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
